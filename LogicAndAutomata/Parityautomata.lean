b07d4149233942d31ef412905388dd31b102bf8f
Riemer Kerkstra <riemer.kerkstra@student.uva.nl>
2025-09-24 15:37:37 +0200

    Define stutterequivalent and stutterclosure. But need to define functiononword

--- LogicAndAutomata/Parityautomata.lean
+++ LogicAndAutomata/Parityautomata.lean
@@ -10,7 +10,13 @@ class NPA A extends NA A where
     FinState: Finite State
     parityMap : State → ℕ
 
+inductive PosNat where
+| one : PosNat
+| succ (n : PosNat) : PosNat
 
+def PosNat.toNat : PosNat → Nat
+| one => Nat.succ Nat.zero
+| succ n => Nat.succ n.toNat
 
 def NPA.ParityAccept (M : NPA A) (as : ℕ → A) :=
 ∃ ss : ℕ → M.State, M.InfRun as ss ∧ Even (sSup ((InfOcc ss).image M.parityMap))
@@ -22,11 +28,15 @@ def NPA.AcceptedOmegaLang (M : NPA A)  : Set (ℕ → A) :=
 def NPA.StutterClosure (M: NPA A) : NPA A :=
 sorry
 
-def SutterEquivalent (w: ℕ → A) (w' : ℕ → A) : Prop :=
+def functiononword (w: ℕ → A) (f : ℕ → (PosNat)) : ℕ → A:=
 sorry
+-- w.foldR (fun )
+
+def StutterEquivalent (w: ℕ → A) (w' : ℕ → A) : Prop :=
+∃ wb : ℕ → A,  ∃ f : ℕ → (PosNat),  ∃ f' : ℕ → (PosNat), w = (functiononword wb f) ∧ w' = (functiononword wb f')
 
 def StutterClosure (L: Set (ℕ → A)) : Set (ℕ → A) :=
-sorry
+{w | ∃ w' ∈ L, (StutterEquivalent w w')}
 
 theorem NA.StutterClosurerecognizesStutterClosure (M : NPA A) :
     (M.StutterClosure).AcceptedOmegaLang = StutterClosure (M.AcceptedOmegaLang) :=
