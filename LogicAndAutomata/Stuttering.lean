b6dbb8d4bbc0cea8817c56b14246671efee38357
Riemer Kerkstra <riemer.kerkstra@student.uva.nl>
2025-11-25 15:03:35 +0100

    Fix naming in Stuttering

--- LogicAndAutomata/Stuttering.lean
+++ LogicAndAutomata/Stuttering.lean
@@ -1,19 +1,17 @@
 import Mathlib
 
-variable {A : Type}
+variable {Alph : Type}
 
-abbrev n_lt_sumk (n:Nat) (f: Stream' ℕ) (k : Nat) : Prop := (n < (∑ m∈ Finset.range (k + 1), (f m + 1)))
+abbrev n_lt_sumk (n : ℕ) (f : Stream' ℕ) (k : Nat) : Prop :=
+       (n < (∑ m∈ Finset.range (k + 1), (f m + 1)))
 
-lemma fstrictmono (f: Stream' ℕ ): StrictMono fun k ↦ (∑ m∈Finset.range k, (f m + 1)) := by
+lemma fstrictmono (f : Stream' ℕ) : StrictMono fun k ↦ (∑ m∈Finset.range k, (f m + 1)) := by
   refine strictMono_nat_of_lt_succ ?_
-  expose_names
   intro m
-  rw [Finset.sum_range_succ]
-  simp only [lt_add_iff_pos_right, add_pos_iff, zero_lt_one, or_true]
+  simp only [Finset.sum_range_succ, lt_add_iff_pos_right, add_pos_iff, zero_lt_one, or_true]
 
-theorem kexists (n:ℕ) (f: Stream' ℕ) : ∃k, (n_lt_sumk n f k) := by
-  let g : Stream' ℕ :=  fun k ↦ (∑ m∈Finset.range k, (f m + 1))
-  have exists_k_above_n: ∃ k, n +1≤ ∑ m ∈ Finset.range (k), (f m + 1) := by
+theorem kexists (n : ℕ) (f : Stream' ℕ) : ∃k, (n_lt_sumk n f k) := by
+  have exists_k_above_n: ∃ k, n + 1 ≤ ∑ m ∈ Finset.range (k), (f m + 1) := by
     apply (Filter.tendsto_atTop_atTop_iff_of_monotone (fstrictmono f).monotone).1
     exact StrictMono.tendsto_atTop (fstrictmono f)
   have h' m := Nat.find_min exists_k_above_n (m := m)
@@ -26,17 +24,20 @@ theorem kexists (n:ℕ) (f: Stream' ℕ) : ∃k, (n_lt_sumk n f k) := by
   simp only [Nat.find_spec exists_k_above_n]
 
 
-def functiononword (w: Stream' A) (f : Stream' ℕ) (n : ℕ) : A :=
+def functiononword (w : Stream' Alph) (f : Stream' ℕ) (n : ℕ) : Alph:=
   let l : ℕ := Nat.find (kexists n f)
   w l
 
-def StutterEquivalent (w: Stream' A) (w' : Stream' A) : Prop :=
-∃ wb : Stream' A,  ∃ f : Stream' Nat,  ∃ f' : Stream' Nat, w = (functiononword wb f) ∧ w' = (functiononword wb f')
+def StutterEquivalent (w : Stream' Alph) (w' : Stream' Alph) : Prop :=
+  ∃ wb : Stream' Alph,  ∃ f : Stream' ℕ,  ∃ f' : Stream' ℕ,
+    w = (functiononword wb f) ∧ w' = (functiononword wb f')
 
-def StutterClosure (L: Set (Stream' A)) : Set (Stream' A) :=
-{w | ∃ w' ∈ L, (StutterEquivalent w w')}
+def StutterClosure (L : Set (Stream' Alph)) : Set (Stream' Alph) :=
+  {w | ∃ w' ∈ L, (StutterEquivalent w w')}
 
-lemma functiononword_eq_base_word {w wb: Stream' A} {b:ℕ} {f: Stream' ℕ} (hw: w = functiononword wb f) (k: ℕ) (hb: b < f k + 1) : w (b + ∑ m ∈ Finset.range k, (f m + 1)) = wb k := by
+lemma functiononword_eq_base_word {w wb : Stream' Alph} {b : ℕ} {f : Stream' ℕ}
+      (hw : w = functiononword wb f) (k : ℕ) (hb : b < f k + 1) :
+      w (b + ∑ m ∈ Finset.range k, (f m + 1)) = wb k := by
   rw [hw]
   unfold functiononword
   simp only
@@ -53,10 +54,13 @@ lemma functiononword_eq_base_word {w wb: Stream' A} {b:ℕ} {f: Stream' ℕ} (hw
     · intro n hn
       if hn2: n < d then
         intro h
-        have bigger: b + ∑ m ∈ Finset.range (d + 1), (f m + 1) > ∑ m ∈ Finset.range (d), (f m + 1) := by rw [Finset.sum_range_succ]; linarith
-        have notnbigger : ¬ (∑ m ∈ Finset.range (d), (f m + 1) < ∑ m ∈ Finset.range (n+1), (f m + 1)) := by
+        have bigger: b + ∑ m ∈ Finset.range (d + 1), (f m + 1) > ∑ m ∈ Finset.range (d), (f m + 1)
+              := by rw [Finset.sum_range_succ]; linarith
+        have notnbigger : ¬ (∑ m ∈ Finset.range (d), (f m + 1) <
+                          ∑ m ∈ Finset.range (n+1), (f m + 1)) := by
           have p0: 0 + ∑ m ∈ Finset.range d, (f m + 1) < ∑ m ∈ Finset.range (d + 1), (f m + 1) ∧
-            ∀ n < d, ¬0 + ∑ m ∈ Finset.range d, (f m + 1) < ∑ m ∈ Finset.range (n + 1), (f m + 1) := by
+               ∀ n < d, ¬0 + ∑ m ∈ Finset.range d, (f m + 1) < ∑ m ∈ Finset.range (n + 1), (f m + 1)
+              := by
             specialize @hd 0
             rw [← (Nat.find_eq_iff (kexists (0 + ∑ m ∈ Finset.range (d), (f m + 1)) f))]
             exact hd (by simp)
@@ -65,6 +69,7 @@ lemma functiononword_eq_base_word {w wb: Stream' A} {b:ℕ} {f: Stream' ℕ} (hw
           simp only [zero_add] at b
           apply b
           exact hn2
+
         exact notnbigger (Nat.lt_trans bigger h)
 
       else
